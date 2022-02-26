a5779d4f0b941eecb6993f258decfe69ed546f5a
Ramon Barros <ramonfilipe@gmail.com>
2022-02-25 23:34:28 -0300

    Exemplo de gráfico

---
+++ GraficoCresPopBrasileira.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 25 23:09:39 2022
+
+@author: Ramon Barros
+
+Criar gráfico mostrando o crescimento populacional brasileiro 1980-2016
+Dados: DataSus
+"""
+import matplotlib.pyplot as plt
+
+dados = open("populacao_brasileira.csv").readlines()
+
+x = []
+y = []
+
+for i in range(len(dados)):
+    if i != 0:
+        linha = dados[i].split(";")
+        x.append(int(linha[0]))
+        y.append(int(linha[1]))
+        
+plt.bar(x, y, color="#87CEFA")
+plt.plot(x, y, color="#00008b", linestyle="-")
+
+plt.title("Crescimento Populacional Brasileiro")
+plt.xlabel("Ano")
+plt.ylabel("População em milhões")
+#plt.show()
+plt.savefig("Grafico População.pdf")
